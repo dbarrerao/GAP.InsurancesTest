fcb02606f95aa2b68df308738a5858a4eb2dc0f3
Diego Alejandro Barrera Ocampo <dbarrerao@dbarrerao.com>
2020-02-08 18:53:49 -0500

    Creation Class ClientBusiness without implementation

---
+++ GAP.InsurancesTest/GAP.Business/Businnes/ClientBusiness.cs
@@ -0,0 +1,31 @@
+﻿using GAP.Business.Interfaces;
+using GAP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GAP.Business.Businnes
+{
+    public class ClientBusiness : IClientBusiness
+    {
+        public void DelClientById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Client> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client GetByDocument(string documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client UpdClientById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
